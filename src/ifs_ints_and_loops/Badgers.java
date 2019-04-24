ffacee4c8d38374252364151bb64dccdc0ec053e
DrOranges6 <league@WTS-iMac-11.attlocal.net>
2019-04-23 18:01:46 -0700

    :cry:

---
+++ src/ifs_ints_and_loops/Badgers.java
@@ -0,0 +1,27 @@
+package ifs_ints_and_loops;
+
+public class Badgers {
+public static void main(String[] args) {
+	String s = "snake";
+	String b = "badger";
+	String m = "mushroom";
+	for (int i = 0; i < 3; i++) {
+		
+	
+	for (int i1 = 0; i1 < 12; i1++) {
+		System.out.print(b+" ");
+	
+		}
+	System.out.print(m+" ");
+	System.out.print(m+" ");
+	System.out.println();
+}
+	{
+}
+int i2 = 0;
+for (int i1 = 0; i2 < 5; i2++) {
+	System.out.print(s+ " ");
+	
+}
+}
+}
---
+++ src/ifs_ints_and_loops/Recipes_html/Recipes_html.pde
@@ -0,0 +1,11 @@
+void setup(){
+
+background(#E310BD);
+for (int i = 0; i < 4; i++) {
+text("ice cream", 10, 10*i);
+
+
+
+}
+text("banana", 10, 40);
+}
